ea6225fe77c58227af0a601d21fb4b01217a43bf
Mauricio Crecencio <65139661+mauriciocrecencio@users.noreply.github.com>
2024-08-28 13:34:57 -0300

    style: add fade-in animation

--- frontend/src/components/Sections/MainSection.tsx
+++ frontend/src/components/Sections/MainSection.tsx
@@ -12,7 +12,7 @@ export default function MainSection() {
         src="/images/work-with-elo7.jpeg"
         className="max-h-[500px] w-full object-cover brightness-[70%]"
       />
-      <Section className="gap-6 md:pt-8">
+      <Section className="animate-slidein gap-6 md:pt-8">
         <p className="px-2 text-center text-gray-500 md:text-2xl">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur et viverra orci. Praesent consequat dolor
           tellus, eget viverra risus hendrerit non. Sed rutrum condimentum maximus. Donec pellentesque libero eu eros
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
@@ -11,13 +8,27 @@ export default {
         'gray-50': '#C8C8CC',
         'gray-100': '#57575C',
         'gray-500': '#333333',
-        'aqua': '#008B90',
+        aqua: '#008B90',
         'light-yellow': '#FFF3DB',
-        'yellow': '#D99F2C',
-        'highlight-yellow': '#FDB933',
+        yellow: '#D99F2C',
+        'highlight-yellow': '#FDB933'
+      },
+      keyframes: {
+        slidein: {
+          from: {
+            opacity: '0',
+            transform: 'translateY(-10px)'
+          },
+          to: {
+            opacity: '1',
+            transform: 'translateY(0)'
+          }
+        }
+      },
+      animation: {
+        slidein: 'slidein 1s ease 300ms'
       }
-    },
+    }
   },
-  plugins: [],
-}
-
+  plugins: []
+};
