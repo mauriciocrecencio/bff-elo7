a71783394494c745dcc3079567714cfbbd0e419e
Mauricio Crecencio <65139661+mauriciocrecencio@users.noreply.github.com>
2024-08-28 21:55:52 -0300

    fix: allow cors for front-end

--- bff/src/server.ts
+++ bff/src/server.ts
@@ -8,6 +8,13 @@ const app = express();
 
 const port = 3000;
 
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  next();
+});
+
 app.use(routes);
 
 app.listen(port, () => {
